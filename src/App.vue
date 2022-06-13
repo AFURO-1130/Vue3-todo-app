88a1cb13c399f3f9a73877dc0b520392ad6b3b99
kinjyo shotaro <abcshotaro616@gmail.com>
2022-06-13 09:56:00 +0900

    first Commit!

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <TodoList />
+</template>
+
+<script lang="ts">
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <input v-model="todoRef" type="text" />
+  <button @click="add">登録</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, SetupContext } from "vue";
+
+export default defineComponent({
+  name: "TodoInput",
+  setup(props, context: SetupContext) {
+    const todoRef = ref<string>("");
+
+    const add = (e) => {
+      context.emit("add-todo", todoRef.value);
+      todoRef.value = "";
+    };
+
+    return { add, todoRef };
+  },
+});
+</script>
